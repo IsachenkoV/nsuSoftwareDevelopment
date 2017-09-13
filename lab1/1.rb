f1e8f6fc9bf4679efa6ed09322c6b850c1f7e1e3
Исаченко Владимир <vv.isachenko@gmail.com>
2017-09-13 21:20:28 +0700

    update global vars

--- lab1/1.rb
+++ lab1/1.rb
@@ -1,16 +1,15 @@
-a = ["a", "b", "c"]
-x = 3
+$a = ["a", "b", "c"]
+$max_sz = 3
 
-def f(ar, n, a)
+def f(ar, n)
   t = []
-  ar.each { |s| t = t + a.map{ |i| s + i }.reject{|i| i[i.length - 1] == i[i.length - 2]} }
-  if (n > 0)
-    return f(t, n - 1, a)
+  ar.each { |s| t = t + $a.map{ |i| s + i }.reject{|i| i[i.length - 1] == i[i.length - 2]} }
+  if (n < $max_sz - 1)
+    return f(t, n + 1)
   else
     return t
   end
 end
 
-b = a;
-puts f(b, x - 1, a)
-
+b = $a;
+puts f(b, 1)
