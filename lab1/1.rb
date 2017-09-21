78ab4dbb07b1b8ef2eb4ac9a27d579ec9770860f
Исаченко Владимир <vv.isachenko@gmail.com>
2017-09-21 22:54:57 +0700

    Remove globals
    remove globals
    update function f, add another way

--- lab1/1.rb
+++ lab1/1.rb
@@ -1,15 +1,15 @@
-$a = ["a", "b", "c"]
-$max_sz = 3
+alp = ["a", 1, [2, "x"]]
+n = 4
 
-def f(ar, n)
-  t = []
-  ar.each { |s| t = t + $a.map{ |i| s + i }.reject{|i| i[i.length - 1] == i[i.length - 2]} }
-  if (n < $max_sz - 1)
-    return f(t, n + 1)
+def f(cur_ar, alp, n)
+  if (n == 0)
+    return cur_ar
   else
-    return t
+    return f(cur_ar.inject([]) { |tmp, s| tmp + alp.map{ |i| s + Array.new(1, i) }.reject{|i| i[-1] == i[-2]} }, alp, n - 1)
   end
 end
 
-b = $a;
-puts f(b, 1)
+b = Array.new(1, [])
+f(b, alp, n).each { |tr| p tr }
+
+(1..n).inject([[]]) { |memo, tmp| memo.flat_map { |s| alp.select { |i| i != s[-1] }.map { |i| s + [i] } } }
