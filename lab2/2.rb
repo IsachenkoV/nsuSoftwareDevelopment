d1512928676b15bb789ef305b96c893182b07090
Исаченко Владимир <vv.isachenko@gmail.com>
2017-09-23 18:39:26 +0700

    Added task#2

---
+++ lab2/2.rb
@@ -0,0 +1,38 @@
+class Array
+  def threading (&func)
+    thread_count = 4
+    if thread_count > size then
+      thread_count = size
+    end
+    part_size = size / thread_count
+    
+    (1..thread_count).map() do |t_ind| 
+      _start = (t_ind - 1) * part_size 
+      _end = t_ind == thread_count ? size : t_ind * part_size
+      part = self[_start..._end]
+      Thread.new(part) { Thread.current[:out] = yield part }.join
+    end.flat_map { |thread| thread[:out] }
+  end
+  
+  def my_all? (&block)
+    result = threading { |part| value = part.all?(&block) }.all?
+  end
+  
+  def my_any? (&block)
+    result = threading { |part| value = part.any?(&block) }.any?
+  end
+  
+  def my_map (&block)
+    result = threading { |part| value = part.map(&block) }
+  end
+  
+  def my_select (&block)
+    result = threading { |part| value = part.select(&block) }
+  end
+end
+
+a = ["asd", "aaa", "aa", "asdasdad", "asdaaa", "asddd"];
+puts a.my_all? {|el| el.length > 3 }
+puts a.my_any? {|el| el.length > 3 }
+p a.my_map {|el| el + "3" }
+p a.my_select {|el| el.length > 3 }
