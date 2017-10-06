596556378f726c19ae652f3cf03f613e4cc850a2
Исаченко Владимир <vv.isachenko@gmail.com>
2017-10-06 20:06:33 +0700

    Fixed encoding (ANSI -> UTF-8)

--- lab3/3.rb
+++ lab3/3.rb
@@ -1,11 +1,19 @@
+﻿strs = File.readlines('details.csv', encoding: 'utf-8')
 
+count_str = "Количество: "
+cost_str = "Стоимость: "
+phone_str = "Номер телефона: "
 
 cost_regexp = '.*;(?<cost>\d+\.?\d*)'
 phone_number_regexp = '(?<phone>\+\d{11})'
 date_regexp = '(?<date>\d{2}\.\d{2}\.\d{2};)'
 
 queries = [
+    ["SMS: ", ".*(SMS|СМС)#{cost_regexp}"],
     ["MMS: ", ".*MMS#{cost_regexp}"],
+    ["Звонки: ", ".*вызовы#{cost_regexp}"],
+    ["Интернет: ", ".*Передача данных#{cost_regexp}"],
+    ["Доп. услуги: ", ".*(Дополнительные|Прочие)#{cost_regexp}"]
 ]
 puts phone_str
 strs.each() {|s| x = s.match(phone_number_regexp); if x != nil then puts x[:phone]; break; end }
